4653145827838f7f19a4167d2d2c5b43316c32bb
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-15 20:40:49 +0000

    Ran migrate

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: deliveries
+#
+#  id                    :integer          not null, primary key
+#  description           :string
+#  details               :string
+#  status                :string
+#  supposed_arrival_date :date
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  user_id               :integer
+#
 class Delivery < ApplicationRecord
 end
